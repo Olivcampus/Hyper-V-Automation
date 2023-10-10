42d8427a3abfdfd68bd0ad18af9e1b5eb59ed76c
Olivcampus <olivier.himblot@le-campus-numerique.fr>
2023-10-10 16:23:16 +0200

    ISS suite

--- testproceed.ps1
+++ testproceed.ps1
@@ -17,14 +17,47 @@ Invoke-Command -Session $sess {
     [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
     Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-    # Install 7-zip
+    # Install soft
     choco install 7zip -y
     choco install mariadb.install -y
     choco install mariadb
     choco install iis.administration -y
-
+    choco install php-manager -y
+    Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebServerRole
+    Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebServer
+    Enable-WindowsOptionalFeature -Online -FeatureName IIS-CommonHttpFeatures
+    Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpErrors
+    Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpRedirect
+    Enable-WindowsOptionalFeature -Online -FeatureName IIS-ApplicationDevelopment
     
+    Enable-WindowsOptionalFeature -online -FeatureName NetFx4Extended-ASPNET45
+    Enable-WindowsOptionalFeature -Online -FeatureName IIS-NetFxExtensibility45
     
+    Enable-WindowsOptionalFeature -Online -FeatureName IIS-HealthAndDiagnostics
+    Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpLogging
+    Enable-WindowsOptionalFeature -Online -FeatureName IIS-LoggingLibraries
+    Enable-WindowsOptionalFeature -Online -FeatureName IIS-RequestMonitor
+    Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpTracing
+    Enable-WindowsOptionalFeature -Online -FeatureName IIS-Security
+    Enable-WindowsOptionalFeature -Online -FeatureName IIS-RequestFiltering
+    Enable-WindowsOptionalFeature -Online -FeatureName IIS-Performance
+    Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebServerManagementTools
+    Enable-WindowsOptionalFeature -Online -FeatureName IIS-IIS6ManagementCompatibility
+    Enable-WindowsOptionalFeature -Online -FeatureName IIS-Metabase
+    Enable-WindowsOptionalFeature -Online -FeatureName IIS-ManagementConsole
+    Enable-WindowsOptionalFeature -Online -FeatureName IIS-BasicAuthentication
+    Enable-WindowsOptionalFeature -Online -FeatureName IIS-WindowsAuthentication
+    Enable-WindowsOptionalFeature -Online -FeatureName IIS-StaticContent
+    Enable-WindowsOptionalFeature -Online -FeatureName IIS-DefaultDocument
+    Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebSockets
+    Enable-WindowsOptionalFeature -Online -FeatureName IIS-ApplicationInit
+    Enable-WindowsOptionalFeature -Online -FeatureName IIS-ISAPIExtensions
+    Enable-WindowsOptionalFeature -Online -FeatureName IIS-ISAPIFilter
+    Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpCompressionStatic
+    
+    Enable-WindowsOptionalFeature -Online -FeatureName IIS-ASPNET45
+    choco install urlrewrite -y
+    choco install webdeploy -y
 }
 
 Remove-PSSession -Session $sess
